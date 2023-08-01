0865257bf764d12f50444b194987a85c4c5e5962
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2023-08-01 09:49:13 +0530

    fix(Hero): changed content of the hero section and header

--- marketing-website/components/hero.tsx
+++ marketing-website/components/hero.tsx
@@ -1,15 +1,32 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
+import VideoThumb from "@/public/images/hero-image.png";
+import ModalVideo from "@/components/modal-video";
+import SlackIcon from "@/public/images/slack.png";
+import HeroImage from "@/public/images/HeroImage.png";
+
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className="relative">
-
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1"
+        aria-hidden="true"
+      >
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
               <stop stopColor="#FFF" offset="0%" />
               <stop stopColor="#EAEAEA" offset="77.402%" />
               <stop stopColor="#DFDFDF" offset="100%" />
@@ -23,39 +40,77 @@ export default function Hero() {
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+            <h1
+              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              Introducing the Ultimate{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                Birthday App{" "}
+              </span>
+              for Slack!
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+              <h2
+                className="text-3xl text-gray-600 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                Never Miss a Birthday Celebration Again!
+              </h2>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                  <a
+                    className="btn text-white bg-black bg-opacity-50 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0 rounded-full flex flex-row items-center"
+                    style={{ lineHeight: 1 }}
+                    href="#0"
+                  >
+                    <Image
+                      className="mr-2"
+                      src={SlackIcon}
+                      width={20}
+                      height="20"
+                      alt="slack"
+                    />
+                    <div className="flex items-center">Add to slack</div>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
 
           {/* Hero image */}
-          <ModalVideo
+          <div className="grid grid-cols-1 md:grid-cols-1 gap-3">
+            <div className="flex items-center justify-center">
+              <Image
+                className="h-auto max-w-full rounded-lg"
+                src={HeroImage}
+                width={800}
+                height="20"
+                alt="slack"
+              />
+            </div>
+          </div>
+
+          {/* <ModalVideo
             thumb={VideoThumb}
             thumbWidth={768}
             thumbHeight={432}
             thumbAlt="Modal video thumbnail"
             video="/videos/video.mp4"
             videoWidth={1920}
-            videoHeight={1080} />
-
+            videoHeight={1080}
+          /> */}
         </div>
-
       </div>
     </section>
-  )
+  );
 }
--- marketing-website/components/ui/logo.tsx
+++ marketing-website/components/ui/logo.tsx
@@ -1,18 +1,42 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        className="w-8 h-8"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <defs>
-          <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
-            <stop stopColor="#4FD1C5" offset="0%" />
-            <stop stopColor="#81E6D9" offset="25.871%" />
-            <stop stopColor="#338CF5" offset="100%" />
+          <radialGradient
+            cx="21.152%"
+            cy="86.063%"
+            fx="21.152%"
+            fy="86.063%"
+            r="79.941%"
+            id="footer-logo"
+          >
+            <stop stopColor="#FC7753" offset="0%" />
+            <stop stopColor="#66d6d1" offset="25.871%" />
+            <stop stopColor="#473d58" offset="100%" />
           </radialGradient>
         </defs>
-        <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
+        <rect
+          width="32"
+          height="32"
+          rx="16"
+          fill="url(#footer-logo)"
+          fillRule="nonzero"
+        />
+        <g transform="translate(6,8) ">
+          <path
+            rx="16"
+            d="M15.44 2.16C14.512 1.264 13.352 0.816 11.96 0.816C11.016 0.816 10.184 1.016 9.464 1.416C8.728 1.816 8.216 2.352 7.928 3.024C7.608 2.384 7.064 1.856 6.296 1.44C5.528 1.024 4.704 0.816 3.824 0.816C1.28 0.816 0.00799847 2.368 0.00799847 5.472V12H3.296V6.384C3.296 5.344 3.44 4.616 3.728 4.2C4 3.768 4.472 3.552 5.144 3.552C6.216 3.552 7.064 3.96 7.688 4.776L7.712 12H10.976V6C10.976 5.088 11.136 4.456 11.456 4.104C11.76 3.736 12.304 3.552 13.088 3.552C13.968 3.552 14.664 3.76 15.176 4.176V12H18.44V1.104H15.656L15.44 2.16Z"
+            fill="white"
+          />
+        </g>
       </svg>
     </Link>
-  )
+  );
 }
