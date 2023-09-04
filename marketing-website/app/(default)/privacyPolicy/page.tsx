152ed7ba8032b32654718c1bc3fe303218c0b9ef
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2023-09-04 22:09:43 +0530

    fix(pages): added some more basic pages to the website

--- marketing-website/app/(default)/privacyPolicy/page.tsx
+++ marketing-website/app/(default)/privacyPolicy/page.tsx
@@ -8,19 +8,16 @@ import remarkHtml from "remark-html";
 
 const PrivacyPolicyPage = () => {
   const getPrivacyPolicyContent = async () => {
-    console.log("1 getStaticProps, loading");
     const filePath = path.join(
       process.cwd(),
       "/app/(default)/privacyPolicy/privacy-policy.md"
     );
-    console.log("2 getStaticProps, loading", filePath);
 
     const fileContents = fs.readFileSync(filePath, "utf8");
     const { content } = grayMatter(fileContents);
 
     const processedContent = await remark().use(remarkHtml).process(content);
     const htmlContent = processedContent.toString();
-    console.log("3 getStaticProps", htmlContent);
 
     return htmlContent;
   };
