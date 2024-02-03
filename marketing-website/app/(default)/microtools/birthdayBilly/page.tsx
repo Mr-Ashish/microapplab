cb56cecd332745671139ec18db907bd6c5f770a2
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2024-02-03 16:35:54 +0530

    feat(birthdayBillyPage): added oauth confirmation page

---
+++ marketing-website/app/(default)/microtools/birthdayBilly/page.tsx
@@ -0,0 +1,15 @@
+// pages/Pricing.js
+
+import Head from "next/head";
+
+const BirthdayBilly = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <Head>
+        <title>Success page</title>
+      </Head>
+    </div>
+  );
+};
+
+export default BirthdayBilly;
