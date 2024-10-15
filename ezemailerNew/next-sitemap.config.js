acc7e92776720818e50c50fb729e5296eaf82e03
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2024-10-15 13:26:06 +0530

    fix(seo): added next-sitemap in my website

---
+++ ezemailerNew/next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://ezemail.microapplab.com", // Replace with your site's URL
+  generateRobotsTxt: true, // (Optional) Generate robots.txt file
+};
--- ezemailerNew/src/app/page.tsx
+++ ezemailerNew/src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+// "use client";
 
 import { Container } from "@/components/Container";
 import { Hero } from "@/components/Hero";
 import { SectionTitle } from "@/components/SectionTitle";
 import { Benefits } from "@/components/Benefits";
-import { Video } from "@/components/Video";
-import { Testimonials } from "@/components/Testimonials";
-import { Faq } from "@/components/Faq";
+// import { Video } from "@/components/Video";
+// import { Testimonials } from "@/components/Testimonials";
+// import { Faq } from "@/components/Faq";
 import { Cta } from "@/components/Cta";
 
 import { benefitOne, benefitTwo } from "@/components/data";
