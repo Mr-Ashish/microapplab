0af51fc658527bcb2d316ba30c3af5a317ae86d5
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2024-10-17 01:26:52 +0530

    fix(website): analytics home

--- marketing-website/app/(default)/layout.tsx
+++ marketing-website/app/(default)/layout.tsx
@@ -6,6 +6,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Footer from "@/components/ui/footer";
+import Script from "next/script";
 
 export default function DefaultLayout({
   children,
@@ -24,6 +25,27 @@ export default function DefaultLayout({
   return (
     <>
       <main className="grow">{children}</main>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WEP4ETGXPN"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WEP4ETGXPN', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <Footer />
     </>
   );
