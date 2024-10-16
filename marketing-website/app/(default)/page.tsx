65be722bf8435f7f6351026c263cc7cb3363a71a
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2024-10-17 01:36:52 +0530

    fix(website): seo keywords

--- marketing-website/app/(default)/page.tsx
+++ marketing-website/app/(default)/page.tsx
@@ -1,12 +1,57 @@
+// app/page.js
+
 export const metadata = {
-  title: "MicroAppLab - BirthdayBilly",
+  title: "MicroAppLab",
+  description:
+    "MicroAppLab offers innovative SaaS tools like BirthdayBilly and Ezemailer to enhance your productivity and streamline workflows.",
+  keywords: [
+    "SaaS",
+    "Software Tools",
+    "Productivity",
+    "BirthdayBilly",
+    "Ezemailer",
+    "Workflow Automation",
+    "Tech Solutions",
+    "Cloud Applications",
+  ],
+  authors: [
+    {
+      name: "Microapplab", // Replace with your actual name or company name
+      url: "https://www.microapplab.com",
+    },
+  ],
+  openGraph: {
+    title: "MicroAppLab ",
+    description:
+      "MicroAppLab offers innovative SaaS tools like BirthdayBilly and Ezemailer to enhance your productivity and streamline workflows.",
+    url: "https://ezemail.microapplab.com/",
+    siteName: "MicroAppLab",
+    images: [
+      {
+        url: "https://www.microapplab.com/images/og-image.jpg", // Replace with your actual image URL
+        width: 1200,
+        height: 630,
+        alt: "MicroAppLab - BirthdayBilly",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MicroAppLab",
+    description:
+      "MicroAppLab offers innovative SaaS tools like BirthdayBilly and Ezemailer to enhance your productivity and streamline workflows.",
+  },
+  robots: "index, follow",
+  icons: {
+    icon: "/favicon.ico", // Ensure this path is correct
+    apple: "/apple-touch-icon.png", // Optional: Add Apple touch icon
+  },
 };
 
 import Hero from "@/components/hero";
-import Features from "@/components/features";
 import FeaturesBlocks from "@/components/features-blocks";
-import Testimonials from "@/components/testimonials";
-import Newsletter from "@/components/newsletter";
 import RequestTool from "@/components/request-tool";
 
 export default function Home() {
